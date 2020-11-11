a4a0055ba327d40db4f0be82296388c38a9396eb
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2020-11-11 10:18:45 +0200

    add posts.php

--- template/posts.php
+++ template/posts.php
@@ -1,15 +1,17 @@
+<?php
+require './posts/posts.php';
+?>
+
 <section class="section">
+
     <h2 class="section__title">Title</h2>
-    <article class="post">
-        <h3 class="post__title">post title</h3>
-        <p class="post__text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    </article>
-    <article class="post">
-        <h3 class="post__title">post title</h3>
-        <p class="post__text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    </article>
-    <article class="post">
-        <h3 class="post__title">post title</h3>
-        <p class="post__text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    </article>
+    <?php foreach ($posts_data as $posts) : ?>
+        <article class="post">
+            <h3 class="post__title">
+                <?= $posts['title']; ?>
+            </h3>
+            <p class="post__text"><?= $posts['body']; ?></p>
+        </article>
+    <?php endforeach; ?>
+
 </section>
